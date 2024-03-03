cf71baa96aea3be86afa86741216af4f41f11a42
Tamirat <amenguda@gmail.com>
2024-03-03 20:29:56 +0300

    page update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
+import Card from '@/components/shared/Card';
 import CategoryFilter from '@/components/shared/CategoryFilter';
-import Collection from '@/components/shared/Collection'
+import Collection from '@/components/shared/Collection';
 import Search from '@/components/shared/Search';
-import { Button } from '@/components/ui/button'
 import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
 import Image from 'next/image'
@@ -17,38 +17,67 @@ export default async function Home({ searchParams }: SearchParamProps) {
     category,
     page,
     limit: 6
-  })
+  });
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
-          <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
-            <Button size="lg" asChild className="button w-full sm:w-fit">
-              <Link href="#events">
-                Explore Now
-              </Link>
-            </Button>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold text-center md:text-left">Trust by</h2>
+
+        <div className="flex md:flex-row justify-between">
+          <div className="w-full md:w-72 mr-4">
+            <CategoryFilter />
+          </div>
+          <div className='flex flex-wrap  '>
+            <div className='flex flex-wrap  '>
+              <div className='w-72 sm:w-1/3 h-auto overflow-hidden transform transition-transform duration-300 hover:scale-105'>
+                <div className='w-full h-screen border p-4'>
+                  <img src="https://i.ibb.co/rv2tPpG/Twitter-header-1.png" alt="Card 1" className="w-full h-80 object-cover mb-4 overflow-hidden border-2" />
+                  <h3 className="text-lg font-bold">Card Title 1</h3>
+                  <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </div>
+              <div className='w-full sm:w-1/3 h-auto overflow-hidden transform transition-transform duration-300 hover:scale-105'>
+                <div className='w-full h-screen border p-4'>
+                  <img src="https://tpc.googlesyndication.com/simgad/15786026862391396112?sqp=4sqPyQQrQikqJwhfEAEdAAC0QiABKAEwCTgDQPCTCUgAUAFYAWBfcAJ4AcUBLbKdPg&rs=AOga4qln_aYiiIwIHnkvHJfYedFrqaOGbg" alt="Card 2" className="w-full  h-80 object-cover mb-4" />
+                  <h3 className="text-lg font-bold">Card Title 2</h3>
+                  <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div className='w-full sm:w-1/3 h-auto overflow-hidden transform transition-transform duration-300 hover:scale-105'>
+                <div className='w-72 h-screen border p-4'>
+                  <img src="https://email2go.io/img/banner.png?v=2" alt="Card 3" className="w-full h-36 object-cover mb-4" />
+                  <h3 className="text-lg font-bold">Card Title 3</h3>
+                  <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div className='w-full sm:w-1/3 h-auto overflow-hidden transform transition-transform duration-300 hover:scale-105'>
+                <div className='w-72 h-screen border p-4'>
+                  <img src="https://i.ibb.co/DR1SS7L/opengraph.jpgg" alt="Card 3" className="w-full h-36 object-cover mb-4" />
+                  <h3 className="text-lg font-bold">Card Title 3</h3>
+                  <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div className='w-full sm:w-1/3 h-auto overflow-hidden transform transition-transform duration-300 hover:scale-105'>
+                <div className='w-72 h-screen border p-4'>
+                  <img src="https://i.ibb.co/DR1SS7L/opengraph.jpgg" alt="Card 3" className="w-full h-36 object-cover mb-4" />
+                  <h3 className="text-lg font-bold">Card Title 3</h3>
+                  <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div className='w-full sm:w-1/3 h-auto overflow-hidden transform transition-transform duration-300 hover:scale-105'>
+                <div className='w-72 h-screen border p-4'>
+                  <img src="https://tpc.googlesyndication.com/simgad/6628671293514050478?sqp=4sqPyQQrQikqJwhfEAEdAAC0QiABKAEwCTgDQPCTCUgAUAFYAWBfcAJ4AcUBLbKdPg&rs=AOga4qkymQMYWu-Xcb2WM5jqbTTG6pfLpA" alt="Card 3" className="w-full h-36 object-cover mb-4" />
+                  <h3 className="text-lg font-bold">Card Title 3</h3>
+                  <p className="text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <Image 
-            src="/assets/images/hero.png"
-            alt="hero"
-            width={1000}
-            height={1000}
-            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-          />
-        </div>
-      </section> 
-
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
-
-        <div className="flex w-full flex-col gap-5 md:flex-row">
-          <Search />
-          <CategoryFilter />
+          <div className="w-full md:w-30 lg:w-80 md:ml-4">
+            <CategoryFilter />
+          </div>
         </div>
 
         <Collection 
@@ -62,5 +91,5 @@ export default async function Home({ searchParams }: SearchParamProps) {
         />
       </section>
     </>
-  )
+  );
 }
